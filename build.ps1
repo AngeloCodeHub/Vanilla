42f2b35750c46908181762b42ff71c93cc999c88
Angelo <a0920217700@gmail.com>
2024-11-24 10:18:54 +0800

    預設使用原生HTML、CSS、JavaScript開發網頁 typescript、tailwincss輔助練習

---
+++ build.ps1
@@ -0,0 +1,2 @@
+bun build .\src\script.ts --outfile=.\dist\script.js
+bun run tailwindcss -i ./src/styles.css -o ./dist/globals.css
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./dist/script.js",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
