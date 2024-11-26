4bb6ec28ccdda3355a6e2ef9426457217909ffed
Angelo <a0920217700@gmail.com>
2024-11-26 18:46:09 +0800

    no goal

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-
 // import _ from 'lodash';
 import './styles/styles.css';
 
 console.log('Hello, Angelo Code World!');
-
-
-// function component() {
-//   const element = document.createElement('div');
-
-//   element.innerHTML = _.join(['Hello', 'Angelo Code World'], ' ');
-//   return element;
-// }
-// document.body.appendChild(component());
